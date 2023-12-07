21e188c6bf8891b8d597e55b0aceb29db5a087c7
alanE33 <>
2023-12-07 15:14:53 +0000

    Deploying to gh-pages from @ alanE33/quick-markdown@3ecef05745d50fe1762ae04a111ed5a8d949ef88 🚀

---
+++ config.js
@@ -0,0 +1,14 @@
+var Config = {
+    docRoot: 'pages/',
+    homepage: 'blog/index.md',
+    defaultTitle: '小蜂的博客2号',
+    notFoundText: '',
+    notFoundPage: '',
+    theme: 'default',
+    showFooterText: true,
+    footerText: 'Blogging by crq'
+}
+
+if (typeof module === 'object') {
+    module.exports = Config
+}
